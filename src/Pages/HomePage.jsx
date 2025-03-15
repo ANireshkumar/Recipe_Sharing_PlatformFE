135c2775e31dcee787be5ca869e94eca6e0e8fe2
ANireshkumar <niresh1606@gmail.com>
2025-03-15 06:34:47 +0530

    register page and login page design done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,8 +3,19 @@ import NavBar from "../components/NavBar";
 const HomePage = () => {
     return (
         <>
-            <NavBar />
-            <h1>HomePage</h1>
+            <div className="container mt-5 text-center">
+                <h1 className="text-4xl text-gray-800">Welcome to Receipe sharing application!</h1>
+                <h4 className="text-xl text-gray-600">A simple Receipe sharing application where you can share your Ideas</h4>
+
+                <div className="max-w-2xl mx-auto mt-5 bg-white shadow-md p-5 rounded-lg overflow-hidden border border-gray-200 px-5 py-5">
+                    <ul className="text-center mt-1 text-lg text-gray-600">
+                        <li>Recipe Sharing</li>
+                        <li>Recipe Discovery</li>
+                        <li>Video Tutorials</li>
+                        <li>Ratings and Comments</li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
