873bc3986b8fc041f18adf2f1ddd12874da0a3a9
ANireshkumar <niresh1606@gmail.com>
2025-03-17 14:20:51 +0530

    dashboard switching --based on role --done

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ import store from "./redux/app/store";
 import DashboardWrapper from "./wrappers/DashboardWrapper";
 import authLoader from "./loaders/unit/authLoader";
 import Logout from "./components/Logout";
+import AdminWrapper from "./wrappers/AdminWrapper";
+import UserDashboard from "./pages/user/UserDashboard";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import UserFeed from "./pages/user/UserFeed";
+import CreatePost from "./pages/user/CreatePost";
 
 const routes = [
   {
@@ -38,6 +43,36 @@ const routes = [
     loader: authLoader,
     hydrateFallbackElement: <p>Loading...</p>,
     children: [
+      {
+        index: true,
+        element: <UserDashboard />
+      },
+      {
+        path: "logout",
+        element: <Logout />,
+        hydrateFallbackElement: <p>Please wait...</p>
+      },
+      {
+        path: "feed",
+        element: <UserFeed />,
+        hydrateFallbackElement: <p>Loading Feed...</p>
+      },
+      {
+        path: "create-post",
+        element: <CreatePost />
+      }
+    ]
+  },
+  {
+    path: "/admin/dashboard",
+    element: <AdminWrapper />,
+    loader: authLoader,
+    hydrateFallbackElement: <p>Loading admin...</p>,
+    children: [
+      {
+        path: "",
+        element: <AdminDashboard />
+      },
       {
         path: "logout",
         element: <Logout />,
